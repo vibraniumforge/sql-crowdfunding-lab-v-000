80610fa79304c25fc39a638e5f3796f9932cb2de
Greg Morgan <gregm9999@gmail.com>
2018-11-21 03:02:58 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 "
 SELECT users.name, SUM (pledges.amount) from users, pledges
 GROUP BY users.name
-ORDER BY amount, user.name
+ORDER BY amount, users.name
 ";
 end
 
