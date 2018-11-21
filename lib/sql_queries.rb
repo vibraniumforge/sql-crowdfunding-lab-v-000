6507f15461b4ed3b328ed1b140073cd2900873a6
Greg Morgan <gregm9999@gmail.com>
2018-11-21 03:02:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,8 +34,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 SELECT users.name, SUM (pledges.amount) from users, pledges
 GGOUP BY users.name
 ORDER BY amount, user.name
-
-"
+";
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
